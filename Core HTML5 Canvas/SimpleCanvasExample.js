395325f211721d7e55fff2980e1e22668cc1ff8d
matthiascy <matthias_cy@outlook.com>
2015-05-08 23:49:29 +0800

    python begins

---
+++ Core HTML5 Canvas/SimpleCanvasExample.js
@@ -0,0 +1,10 @@
+// SimpleCanvasExample.js
+
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+context.font = "38pt Arial";
+context.fillStyle = "cornflowerblue";
+context.strokeStyle = "blue";
+context.fillText("Hello Canvas", canvas.width/2 - 150, canvas.height/2 + 15);
+context.strokeText("Hello Canvas", canvas.width/2 - 150, canvas.height/2 + 15);
