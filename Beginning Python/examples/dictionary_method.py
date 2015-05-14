c45b4eabc805ac5d2878a7e24a387c9382039853
matthiascy <matthias_cy@outlook.com>
2015-05-14 21:46:43 +0800

    condition python

---
+++ Beginning Python/examples/dictionary_method.py
@@ -0,0 +1,41 @@
+# A simple database using get()
+
+# Insert database (people) from dictionary.py
+
+people = {
+    'Alice': {
+        'phone': '2341',
+        'addr': 'Foo drive 23'
+    },
+
+    'Beth': {
+        'phone': '9102',
+        'addr': 'Bar street 42'
+    },
+
+    'Cecil': {
+        'phone': '3158',
+        'addr': 'Baz avenue 90'
+    }
+}
+
+labels = {
+    'phone': 'phone number',
+    'addr': 'address'
+}
+
+name = raw_input('Name: ')
+
+# Are we looking for a phone number or an address?
+request = raw_input('Phone number (p) or address (a)? ')
+
+# Use the correct key:
+if request == 'p': key = 'phone'
+if request == 'a': key = 'addr'
+
+# Use the get to provide default values:
+person = people.get(name, {})
+label = labels.get(key, key)
+result = person.get(key, 'not available')
+
+print "%s's %s %s." % (name, label, result)
